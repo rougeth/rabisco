2090a5e40fcdaeae8dbf84b01780b2d3c1897db0
Marco Rougeth <marco@rougeth.com>
2018-03-31 21:33:30 -0300

    Add commands descriptions

--- cli.py
+++ cli.py
@@ -8,18 +8,21 @@ from rabisco import Rabisco, get_default_path
 @click.option('--path', envvar='RABISCO_PATH', default=get_default_path())
 @click.pass_context
 def cli(ctx, path):
+    """Rabisco"""
     ctx.obj = Rabisco(path=path)
 
 
 @cli.command()
 @click.pass_obj
 def mk(rabisco):
+    """Create a note"""
     rabisco.mk()
 
 
 @cli.command()
 @click.pass_obj
 def ls(rabisco):
+    """List notes"""
     rabisco.ls()
 
 
@@ -27,6 +30,7 @@ def ls(rabisco):
 @click.argument('id', type=click.INT)
 @click.pass_obj
 def rm(rabisco, id):
+    """Remove a note"""
     rabisco.rm(id)
 
 
@@ -34,4 +38,5 @@ def rm(rabisco, id):
 @click.argument('id', type=click.INT)
 @click.pass_obj
 def open(rabisco, id):
+    """Open a note to edition"""
     rabisco.open(id)
